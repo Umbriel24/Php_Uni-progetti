70657dcc70a144958fe28c71e8e3822341a6d6a4
Umberto <umberto.gargi24@gmail.com>
2025-04-03 15:54:07 +0200

    Stampa di convogli liberi e locomotive libere

---
+++ progetto1/ComandiSQL/creazioneConvoglio.php
@@ -0,0 +1,66 @@
+﻿<?php
+require __DIR__ . '/../CartellaDB/database.php';
+
+//GET
+function getCarrozzeByAttivita($attivita)
+{
+    $query = "SELECT * FROM progetto1_Carrozza WHERE in_attività = '$attivita'";
+    return EseguiQuery($query);
+}
+
+function getLocomotriceByAttivita($attivita)
+{
+    $query = "SELECT * FROM progetto1_ComposizioneLocomotrice as c
+            LEFT JOIN progetto1_Locomotiva as l on c.riferimentoLocomotiva = l.id_locomotrice
+            LEFT JOIN progetto1_Automotrice as a on c.riferimentoAutomotiva = a.id_automotrice
+            WHERE in_attività = '$attivita'";
+    return EseguiQuery($query);
+}
+
+
+//Funzioni echo
+function stampaCarrozzeInattive($carrozeInattive)
+{
+    if ($carrozeInattive != null && $carrozeInattive->RecordCount() > 0) {
+        echo '<table>';
+        echo '<tr><th>ID</th><th>Numero di serie</th><th>Posti</th></tr>';
+
+        //corrisponde al foreach di c#
+        while ($row = $carrozeInattive->FetchRow()) {
+            echo '<tr>';
+            echo '<td>' . $row['codice_carrozza'] . '</td>';
+            echo '<td>' . $row['numero_di_serie'] . '</td>';
+            echo '<td>' . $row['posti_a_sedere'] . '</td>';
+            echo '</tr>';
+        }
+
+        echo '</table>';
+    } else {
+        echo '<div class="alert">Nessuna convoglio inattivo.</div>';
+    }
+}
+
+function stampaLocomotriciInattive($locomotriciInattive){
+    if ($locomotriciInattive != null && $locomotriciInattive->RecordCount() > 0) {
+        echo '<table>';
+        echo '<tr><th>ID</th><th>Numero di serie</th><th>Posti</th></tr>';
+
+        while ($row = $locomotriciInattive->FetchRow()) {
+            if($row['id_locomotrice'] != null){
+                //Locomotiva senza posti
+                echo '<tr>';
+                echo '<td>' . $row['codice_Locomotiva'] . '</td>';
+                echo '<td>' . $row['nome'] . '</td>';
+                echo '<td>' . '0' . '</td>';
+                echo '</tr>';
+            } else if ($row['id_automotrice'] != null){
+                echo '<tr>';
+                echo '<td>' . $row['codice_automotrice'] . '</td>';
+                echo '<td>' .'N/A' . '</td>';
+                echo '<td>' . $row['posti_a_sedere'] . '</td>';
+                echo '</tr>';
+            }
+        }
+    }
+}
+
--- progetto1/esercizio.php
+++ progetto1/esercizio.php
@@ -1,1 +1,31 @@
 ﻿<?php
+require 'ComandiSQL/creazioneConvoglio.php';
+//Compone e scompone convogli
+//Costruisce le corse con le tratte e orari.
+//Ogni corsa può essere modificata o cancellata.
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css">
+    <title>Società ferrovie Turistiche - Account esercizio SFT</title>
+
+</head>
+<body>
+<div class="container">
+    <h1>Account backoffice esercizio</h1>
+    <p>Ecco i convogli liberi:</p>
+    <div>
+        <?php stampaCarrozzeInattive(getCarrozzeByAttivita('No'));?>
+    </div>
+
+    <p>Ecco le locomotive libere:</p>
+    <?php stampaLocomotriciInattive(getLocomotriceByAttivita('No'));?>
+</div>
+</body>
+</html>
