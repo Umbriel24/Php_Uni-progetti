167284953c1e47a84703a87efc5f01a992a50475
Umberto <umberto.gargi24@gmail.com>
2025-04-09 23:18:00 +0200

    commit

---
+++ config.php
@@ -0,0 +1,19 @@
+﻿<?php
+$envPath = __DIR__ . '/../.env'; // modifica se il path è diverso
+
+if (file_exists($envPath)) {
+    $lines = file($envPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0 || strpos($line, '=') === false) continue;
+
+        list($name, $value) = explode('=', $line, 2);
+        $name = trim($name);
+        $value = trim($value);
+
+        // Elimina eventuali doppi apici
+        $value = trim($value, '"');
+
+        $_ENV[$name] = $value;
+        putenv("$name=$value"); // utile anche per getenv()
+    }
+}
--- progetto1/CartellaDB/database.php
+++ progetto1/CartellaDB/database.php
@@ -1,5 +1,7 @@
 ﻿<?php
+include __DIR__ . '/../../config.php';
 include  __DIR__ . '/../ADOdb-5.22.8/adodb.inc.php';
+
 //Staticamente otteniamo il $db
 
 if(!function_exists('getConnessioneDb')){
@@ -9,6 +11,7 @@ if(!function_exists('getConnessioneDb')){
     date_default_timezone_set('Europe/Rome');
 
     function getConnessioneDb(){
+
         static $db = null;
 
         if($db === null){
